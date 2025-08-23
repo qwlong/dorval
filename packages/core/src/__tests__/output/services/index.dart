832c281c4cfc41b31cf849d9d4f487683f8b08d3
David <wenlongqi@gmail.com>
2025-08-24 02:14:11 +0800

    feat: polish the codes

--- packages/core/src/__tests__/output/services/index.dart
+++
@@ -1,6 +0,0 @@
-// Generated index file for services
-export 'pets_service.dart';
-export 'pets_pets_nested_array_service.dart';
-export 'models/params/list_pets_params.f.dart';
-export 'models/params/list_pets_nested_array_params.f.dart';
-export 'models/params/index.dart';
