1f6f3a358e438e7270952a711a06e48600798d5f
David <wenlongqi@gmail.com>
2025-09-03 06:42:36 +0800

    feat: polish the codes

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,5 +1,3 @@
 export * from './file';
 export * from './type-mapper';
 export * from './assertion';
-// ReferenceResolver is in resolvers directory, not utils
-export { ReferenceResolver } from '../resolvers/reference-resolver';
